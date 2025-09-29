907853fe42e566d6449c57ec7d873f7639a71502
dparitosh <paritoshdesh@gmail.com>
2025-09-29 06:57:15 +0530

    scrubbed
    Signed-off-by: dparitosh <paritoshdesh@gmail.com>

--- start-backend-no-reload.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Backend Service (No Reload)...
-cd /d "D:\Software\boiSoftware\neoboi\backend"
-set PYTHONPATH=D:\Software\boiSoftware\neoboi
-echo PYTHONPATH set to: %PYTHONPATH%
-echo Current directory: %CD%
-python -m uvicorn main:app --host 127.0.0.1 --port 3001
-pause
--- start-backend-simple.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Backend Service...
-cd /d "D:\Software\boiSoftware\neoboi\backend"
-set PYTHONPATH=D:\Software\boiSoftware\neoboi
-echo PYTHONPATH set to: %PYTHONPATH%
-echo Current directory: %CD%
-python -m uvicorn main:app --host 127.0.0.1 --port 3001 --reload
-pause
